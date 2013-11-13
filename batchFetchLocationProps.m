235f24ddca9a3467486731834b70c570682f9116
ackman678 <james.ackman@gmail.com>
2013-11-13 11:15:36 -0500

    Integrated stimulus based dataset ability into batchFetchLocationProps

--- batchFetchLocationProps.m
+++ batchFetchLocationProps.m
@@ -1,9 +1,11 @@
-function batchFetchLocationProps(filelist,region, datafilename)
-%wbatchFetchLocationProps - A wrapper and output generator for getting information on active pixel fraction per location and by 'active' and 'non-active' periods during the movie, after 'locationData' data structure has been returned and saved into 'region' from wholeBrain_activeFraction.m
-%James B. Ackman, 2013-04-11 12:17:14
+function batchFetchLocationProps(filelist,region, datafilename, useStimuli, stimuliIndices)
+%batchFetchLocationProps - A wrapper and output generator for getting information on active pixel fraction per location during the movie, after 'locationData' data structure has been returned and saved into 'region' from wholeBrain_activeFraction.m
 %Examples:
 % >> batchFetchLocationProps(filelist);
 % >> batchFetchLocationProps({filename},region);
+% >> batchFetchLocationProps(filelist,[],[], 'true', {'motor.state.active' 'motor.state.quiet'});
+% >> batchFetchLocationProps({fnm},[],[], 'true', {'motor.state.active' 'motor.state.quiet' 'drug.state.control' 'drug.state.isoflurane'});
+% >> batchFetchLocationProps({filename},region,'dLocationProps.txt', 'true', [2 3]);
 %
 %**USE**
 %Must provide one input:
@@ -16,25 +18,32 @@ function batchFetchLocationProps(filelist,region, datafilename)
 %(2) a single filename (filename of your region .mat file) as a cell array, i.e.  {filename}
 %
 %Options:
-%filelist={filename}; %can pass just a single filename and a single already loaded region structure, if only getting values for a single file.
+%filelist={filename}; % cell array of strings, can pass just a single filename and a single already loaded region structure, if only getting values for a single file.
+%region - datastructure, if you just want to do a single file loaded into workspace
+%datafilename - string, append data to prexisting table with filename 'datafilename'
+%useStimuli - string, 'true' | 'false'
+%stimuliIndices - integer vector of stimulus indices or a cell array of strings of stimulus descriptions for selecting stimuli in your region.stimuli data structure
 %
 %Output:
 %Right now this function will automatically write to a space-delimited txt file outputs, a 'region.location/active period type' based dataset 'dLocationProps.txt'
 %And these outputs will be appended if the file already exists.
 %
 % See also wholeBrain_getActiveFractionPeriods.m, wholeBrain_activeFraction.m, batchFetchStimResponseProps, batchFetchCalciumEventProps.m
+%
+%James B. Ackman, 2013-04-11 12:17:14, updated 2013-11-13 10:08:46
 
 %-----------------------------------------------------------------------------------------
 %- Set up options and default parameters
 %-----------------------------------------------------------------------------------------
+
+
+if nargin< 5 || isempty(stimuliIndices); stimuliIndices = []; end 
+if nargin< 4 || isempty(useStimuli); useStimuli = 'false'; end
 if nargin< 3 || isempty(datafilename); datafilename = 'dLocationProps.txt'; end
 if nargin< 2 || isempty(region); region = []; end
 
-
-%@nMaskPixels @nPixelsActiveTotal @nPixelsActive @nPixelsActivePerSec  new 2013-11-12 17:15:25
-
 %---**functionHandles.workers and functionHandles.main must be valid functions in this program or in matlabpath to provide an array of function_handles
-functionHandles.workers = {@filename @matlab_filename @region_name @actvPeriodType @actvFraction @maxFraction @minFraction @meanFraction @sdFraction @meanActvFraction @sdActvFraction @actvFrames @actvTimeFraction @nonActvFrames @nonActvTimeFraction @maxDuration_s @minDuration_s @medianDuration_s @meanDuration_s @sdDuration_s @sumDuration_s};
+functionHandles.workers = {@filename @matlab_filename @region_name @actvFraction @maxFraction @minFraction @meanFraction @sdFraction @meanActvFraction @sdActvFraction @actvFrames @actvTimeFraction @nonActvFrames @nonActvTimeFraction @stimulusDesc @nstimuli @stimOn @stimOff @nMaskPixels @nPixelsActiveTotal @nPixelsActive @nPixelsActivePerSec};
 functionHandles.main = @wholeBrain_getActiveFractionStats;
 %tableHeaders = {'filename' 'matlab.filename' 'region.name' 'roi.number' 'nrois' 'roi.height.px' 'roi.width.px' 'xloca.px' 'yloca.px' 'xloca.norm' 'yloca.norm' 'freq.hz' 'intvls.s' 'onsets.s' 'durs.s' 'ampl.df'};
 %filename %roi no. %region.name %roi size %normalized xloca %normalized yloca %region.stimuli{numStim}.description %normalized responseFreq %absolutefiringFreq(dFreq) %meanLatency %meanAmpl %meanDur
@@ -47,8 +56,7 @@ datafilename=setupDataTable(tableHeaders, datafilename);
 
 %---Generic main function loop-------------------
 %Provide valid function handle
-mainfcnLoop(filelist, region, datafilename, functionHandles)
-end
+mainfcnLoop(filelist, region, datafilename, functionHandles, useStimuli, stimuliIndices)
 
 
 function datafilename=setupDataTable(tableHeaders, datafilename)
@@ -61,7 +69,6 @@ if setupHeaders < 1
 	%write headers to file----
 	appendCellArray2file(datafilename,tableHeaders)
 end
-end
 
 
 function appendCellArray2file(filename,output)
@@ -73,10 +80,9 @@ tmp2=tmp';
 fprintf(fid,[repmat('%s\t',1,size(tmp2,1)-1),'%s\n'],tmp2{:});  %tab delimited
 %fprintf(fid,[repmat('%s ',1,size(tmp2,1)-1),'%s\n'],tmp2{:});  %space delimited
 fclose(fid);
-end
 
 
-function mainfcnLoop(filelist, region, datafilename, functionHandles)
+function mainfcnLoop(filelist, region, datafilename, functionHandles, useStimuli, stimuliIndices)
 %start loop through files-----------------------------------------------------------------
 
 if nargin< 2 || isempty(region); 
@@ -108,6 +114,26 @@ for j=1:numel(fnms)
     end
 	[pathstr, name, ext] = fileparts(fnms{j});
 	region.matfilename = [name ext];  %2012-02-07 jba    
+
+	if strcmp(useStimuli,'true') & isempty(stimuliIndices) & isfield(region,'stimuli'); 
+		stimuliIndices=1:numel(region.stimuli);
+	elseif strcmp(useStimuli,'true') & iscellstr(stimuliIndices) & isfield(region,'stimuli')  %if the input is a cellarray of strings
+			ind = [];
+			for i = 1:length(region.stimuli)
+				for k = 1:length(stimuliIndices)
+					if strcmp(region.stimuli{i}.description,stimuliIndices{k})
+						ind = [ind i];
+					end
+				end
+			end
+			stimuliIndices = ind; %assign indices 
+	elseif strcmp(useStimuli,'true') & isnumeric(stimuliIndices) & isfield(region,'stimuli')
+		return
+	elseif ~isfield(region,'stimuli') || strcmp(useStimuli,'false')
+		stimuliIndices = [];
+	else
+		error('Bad input to useStimuli, stimuliIndices, or region.stimuli missing')
+	end
 	
 %	rowinfo = [name1 name2];  %cat cell array of strings
 %	rowinfo = filelist(j,:);
@@ -115,18 +141,17 @@ for j=1:numel(fnms)
 
     disp('--------------------------------------------------------------------')
 	%myEventProps(region,rowinfo);
-	functionHandles.main(region, functionHandles.workers, datafilename)
+	functionHandles.main(region, functionHandles.workers, datafilename, stimuliIndices)
     h = waitbar(j/numel(fnms));
 end
 %data=results;
 close(h)
-end
 
 
 
 %-----------------------------------------------------------------------------------------
 %dataFunctionHandle
-function output = wholeBrain_getActiveFractionStats(region, functionHandles, datafilename)
+function output = wholeBrain_getActiveFractionStats(region, functionHandles, datafilename, stimuliIndices)
 %script to fetch the active and non-active pixel fraction period durations
 %for all data and all locations
 %2013-04-09 11:35:04 James B. Ackman
@@ -136,32 +161,38 @@ function output = wholeBrain_getActiveFractionStats(region, functionHandles, dat
 
 locationMarkers = unique(region.location);
 varin.datafilename=datafilename;
+varin.region=region;
 
-%SignalMatrix = zeros(length(locationMarkers),size(region.locationData.data(1).activeFractionByFrame,2));  %for making a combined actvFraction location signal
-for locationIndex = 1:length(locationMarkers)
-    locationName = region.locationData.data(locationIndex).name;
-    rawSignal = region.locationData.data(locationIndex).activeFractionByFrame;
-    %START Add for loop here by stimulus.stimuliParams-------------------------------
-    pulseSignal = makeActivePulseSignal(rawSignal);
-    plotTitles{1} = ['active fraction by frame for ' locationName]; plotTitles{2} = 'active periods to positive pulse'; plotTitles{3} = 'derivative of active pulse';
-    [onsets, offsets] = getPulseOnsetsOffsets(rawSignal,pulseSignal,plotTitles,locationName);
-    varin.onsets=onsets;
-    varin.offsets=offsets;
-    varin.locationName=locationName;
-    varin.region=region;
-    varin.periodType='active';
-    varin.locationIndex=locationIndex;
-    printStats(functionHandles, varin) 
-    
-    pulseSignal = makeNonActivePulseSignal(rawSignal);
-    plotTitles{1} = ['active fraction by frame for ' locationName]; plotTitles{2} = 'Non-active periods to positive pulse'; plotTitles{3} = 'derivative of non-active pulse';
-    [onsets, offsets] = getPulseOnsetsOffsets(rawSignal,pulseSignal,plotTitles,locationName);
-    varin.onsets=onsets;
-    varin.offsets=offsets;
-    varin.periodType='non.active';
-    printStats(functionHandles, varin)
-    %END Add for loop here by stimulus.stimuliParams-------------------------------    
-%    SignalMatrix(locationIndex,:) = rawSignal; %for making a combined actvFraction location signal
+if ~isempty(stimuliIndices)
+	%SignalMatrix = zeros(length(locationMarkers),size(region.locationData.data(1).activeFractionByFrame,2));  %for making a combined actvFraction location signal
+	for locationIndex = 1:length(locationMarkers)
+		locationName = region.locationData.data(locationIndex).name;
+		%START For loop here by stimulus.stimuliParams-------------------------------
+		for numStim = stimuliIndices
+			for nstimuli=1:numel(region.stimuli{numStim}.stimulusParams)
+				varin.nstimuli = nstimuli;
+				varin.stimulusdesc = region.stimuli{numStim}.description;
+				varin.on = region.stimuli{numStim}.stimulusParams{nstimuli}.frame_indices(1);
+				varin.off = region.stimuli{numStim}.stimulusParams{nstimuli}.frame_indices(end);
+				varin.locationName=locationName;
+				varin.locationIndex=locationIndex;
+				printStats(functionHandles, varin) 
+			end
+		end
+		%END For loop here by stimulus.stimuliParams-------------------------------
+	%    SignalMatrix(locationIndex,:) = rawSignal; %for making a combined actvFraction location signal
+	end
+else
+	for locationIndex = 1:length(locationMarkers)
+		locationName = region.locationData.data(locationIndex).name;
+		varin.nstimuli = '';
+		varin.stimulusdesc = '';
+		varin.on = 1;
+		varin.off = numel(region.locationData.data(locationIndex).nPixelsByFrame);
+		varin.locationName=locationName;
+		varin.locationIndex=locationIndex;
+		printStats(functionHandles, varin) 
+	end
 end
 %{
 %---START---use the combined actvFraction location signal----   
@@ -193,7 +224,7 @@ varin.periodType='non.active';
 printStats(functionHandles, varin)
 %---END---use the combined actvFraction location signal----
 %}
-end
+
 
 function stats = printStats(functionHandles, varin)
 output=cellfun(@(x)x(varin), functionHandles, 'UniformOutput', false); %cellfun example using a generic function  @x applied to the function cell array so that varin can be passed to each function
@@ -205,156 +236,176 @@ appendCellArray2file(varin.datafilename,output)
 %do for loop through functionHandles or cellfun
 %S = cellfun(@str2func, {'sin' 'cos' 'tan'}, 'UniformOutput', false);  %cellfun example
 %cellfun(@(x)x(2), S, 'UniformOutput', false) %cellfun example
-end
+
 
 
 function out = filename(varin) 
 %movie .tif filename descriptor string
 out = varin.region.filename;
-end
+
 
 function out = matlab_filename(varin)
 %analysed .mat file descriptor string
 out = varin.region.matfilename;
-end
+
 
 %------------active fraction stats functions---------------------
 function out = region_name(varin)
 %location name descriptor string
 out = varin.locationName;
-end
+
 
 function out = actvPeriodType(varin)  
 %active period type descriptor string
 out = varin.periodType;
-end
+
 
 function out = actvFraction(varin)  
 %fraction of all pixels active by location for one recording
 data = varin.region.locationData.data;
 out = data(varin.locationIndex).activeFraction;
-end
+
+
+function out = stimulusDesc(varin)
+data = varin.region.locationData.data;
+out = varin.stimulusdesc;
+
+
+function out = nstimuli(varin)
+data = varin.region.locationData.data;
+out = varin.nstimuli;
+
+
+function out = stimOn(varin) 
+data = varin.region.locationData.data;
+out = varin.on;
+
+
+function out = stimOff(varin)
+data = varin.region.locationData.data;
+out = varin.off;
+
 
 function out = nMaskPixels(varin)
 data = varin.region.locationData.data;
 out = data(varin.locationIndex).nPixels;
-end
+
 
 function out = nPixelsActiveTotal(varin)
 data = varin.region.locationData.data;
 out = sum(data(varin.locationIndex).nPixelsByFrame);
-end
+
 
 function out = nPixelsActive(varin)
 data = varin.region.locationData.data;
 out = sum(data(varin.locationIndex).nPixelsByFrame(varin.on:varin.off));
-end
+
 
 function out = nPixelsActivePerSec(varin)
 data = varin.region.locationData.data;
 out = sum(data(varin.locationIndex).nPixelsByFrame(varin.on:varin.off)) / (numel(varin.on:varin.off)*varin.region.timeres);
-end
+
 
 function out = maxFraction(varin)  
 %maximum fraction of all pixels active at one time (default is by frame, TODO: change/add in future by binned time?)
 data = varin.region.locationData.data;
 out = max(data(varin.locationIndex).activeFractionByFrame);
-end
+
 
 function out = minFraction(varin)  
 %minimum fraction of all pixels active at one time
 data = varin.region.locationData.data;
 out = min(data(varin.locationIndex).activeFractionByFrame);
-end
+
 
 function out = meanFraction(varin)
 %mean active pixel fraction across *all* frames
 data = varin.region.locationData.data;
 out = mean(data(varin.locationIndex).activeFractionByFrame);
-end
+
 
 function out = sdFraction(varin)
 %standard deviation active pixel fraction across *all* frames
 data = varin.region.locationData.data;
 out = std(data(varin.locationIndex).activeFractionByFrame);
-end
+
 
 function out = meanActvFraction(varin)
 %mean pixel fraction across all the *active* frames
 data = varin.region.locationData.data;
 actvFramesIdx = find(data(varin.locationIndex).activeFractionByFrame);
 out = mean(data(varin.locationIndex).activeFractionByFrame(actvFramesIdx));
-end
+
 
 function out = sdActvFraction(varin)
 %standard deviation pixel fraction across all the *active* frames
 data = varin.region.locationData.data;
 actvFramesIdx = find(data(varin.locationIndex).activeFractionByFrame);
 out = std(data(varin.locationIndex).activeFractionByFrame(actvFramesIdx));
-end
+
 
 function out = actvFrames(varin)
 %number of active frames
 data = varin.region.locationData.data;
 out = numel(find(data(varin.locationIndex).activeFractionByFrame));
-end
+
 
 function out = actvTimeFraction(varin)
 %fraction of total movie time the location is active
 data = varin.region.locationData.data;
 out = actvFrames(varin)/length(data(varin.locationIndex).activeFractionByFrame);
-end
+
 
 function out = nonActvFrames(varin)
 %number of non-active frames
 data = varin.region.locationData.data;
 out = length(data(varin.locationIndex).activeFractionByFrame) - actvFrames(varin);
-end
+
 
 function out = nonActvTimeFraction(varin)
 %fraction of total movie time the location is non-active
 data = varin.region.locationData.data;
 out = nonActvFrames(varin)/length(data(varin.locationIndex).activeFractionByFrame);
-end
+
 
 %------------actvPeriod duration stats functions---------------------
 function out = maxDuration_s(varin)
 out = max((varin.offsets-varin.onsets).*varin.region.timeres);
-end
+
 
 function out = minDuration_s(varin)
 out = min((varin.offsets-varin.onsets).*varin.region.timeres);
-end
+
 
 function out = medianDuration_s(varin)
 out = median((varin.offsets-varin.onsets).*varin.region.timeres);
-end
+
 
 function out = meanDuration_s(varin)
 out = mean((varin.offsets-varin.onsets).*varin.region.timeres);
-end
+
 
 function out = sdDuration_s(varin)
 out = std((varin.offsets-varin.onsets).*varin.region.timeres);
-end
+
 
 function out = sumDuration_s(varin)
 out = sum((varin.offsets-varin.onsets).*varin.region.timeres);
-end
+
 
 
 %------------Find active period functions---------------
 function pulseSignal = makeActivePulseSignal(rawSignal)
 pulseSignal = rawSignal;
 pulseSignal(rawSignal>0) = 1;
-end
+
 
 function pulseSignal = makeNonActivePulseSignal(rawSignal)
 pulseSignal = rawSignal;
 pulseSignal(rawSignal>0) = -1;
 pulseSignal(pulseSignal>-1) = 1;
 pulseSignal(pulseSignal<1) = 0;
-end
+
 
 function [wvonsets, wvoffsets] = getPulseOnsetsOffsets(rawSignal,pulseSignal,plotTitles,locationName,makePlots)
 if nargin < 5 || isempty(makePlots), makePlots = 0; end
@@ -407,5 +458,4 @@ if makePlots > 0
 	hold on
 	plot(wvonsets,dx2(wvonsets),'og');
 	plot(wvoffsets,dx2(wvoffsets),'or');
-end
 end
